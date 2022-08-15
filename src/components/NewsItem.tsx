22be6638a231c246d7fe8b5ea9a89f210826d967
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-08-15 16:44:48 -0500

    styling news items and news page

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import Image from 'next/image';
+import Image from 'next/image'
+import styles from '../styles/NewsItem.module.css'
 
 function NewsItem({news}: {news: any}) {
     return (
-        <div>
-            <p>News</p>
+        <div className={styles.newsContainer}>
+            <p className={styles.header}>News</p>
             {news && news.map((news: {
                 id: React.Key;
                 date: string;
+                time: string;
                 title: string;
                 description: string;
                 image: string}) => (
-                <div key={news.id}>
-                    <div>
-                        <h2>{news.date}</h2>
-                        <h1>{news.title}</h1>
+                <div className={styles.main} key={news.id}>
+                    <div className={styles.generalInfo}>
+                        <h2 className={styles.dateTime}>{news.date}</h2>
+                        <h2 className={styles.dateTime}>{news.time}</h2>
+                        <h1 className={styles.title}>{news.title}</h1>
                     </div>
-                    <h2>{news.description}</h2>
+                    <h2 className={styles.description}>{news.description}</h2>
                     <Image
                         src={news.image}
                         alt={news.title}
