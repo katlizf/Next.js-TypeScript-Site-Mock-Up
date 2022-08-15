6506a3fd871dd773b633983cd431b1a4f62e0e3c
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-08-15 12:23:57 -0500

    starting team member component

---
+++ src/components/TeamMember.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Data = {
+    name: string,
+    title: string,
+    statement: string
+}
+
+function TeamMember() {
+    return (
+        <div>
+            <img />
+            <h1>Name</h1>
+            <h2>Title</h2>
+            <h2>Statement</h2>
+        </div>
+    )
+}
+
+export default TeamMember
