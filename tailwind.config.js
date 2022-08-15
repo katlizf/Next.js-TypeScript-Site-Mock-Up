383e56e29bc056698f2eee070c3644c84d00de8e
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-08-15 12:34:05 -0500

    installed postcss and editted logo + other files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,tsx, ts}"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
         extend: {},
         colors: {
