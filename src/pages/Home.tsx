70bf62293047332ae65c20d0ff3cf9e891fdc467
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-08-15 12:23:26 -0500

    file strcture change

---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Background from '../images/home-bground.jpg'
+
+function Home() {
+	return (
+		<div className={styles.main}>
+			<span className={styles.mission}>
+				<p className={styles.title}>Our Mission</p>
+				<br />
+				<p className={styles.statement}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis doloremque, soluta velit id sint natus hic vero tempore. Et reprehenderit optio distinctio consequuntur iste porro. Laborum aliquid perspiciatis quo rem!</p>
+			</span>
+			<Image
+				src={Background}
+				alt='DNA/Cells'
+				className={styles.image}
+				layout='responsive'
+			/>
+		</div>
+	)
+}
+
+export default Home
---
+++ src/pages/OurTeam.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import TeamMember from '../components/TeamMember'
+
+function OurTeam() {
+    return (
+        <div>
+            <Navbar />
+            <TeamMember />
+            <Footer />
+        </div>
+    )
+}
+
+export default OurTeam
