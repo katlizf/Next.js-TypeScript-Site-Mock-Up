270a1c338b735f690f240a81f0fcbd147c4cddfa
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-08-15 09:55:08 -0500

    working on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../styles/Navbar.module.css'
 
 function Navbar() {
     return (
         <div>
             {/* <Image src={'/images/ttt-logo.png'} width={250} height={200} alt='Logo' /> */}
+            <div className={styles.nav}>
+                <Link href='/'>
+                    <button className={styles.navItem}>Home</button>
+                </Link>
+                <Link href='/ourteam'>
+                    <button className={styles.navItem}>Our Team</button>
+                </Link>
+                <Link href='/services'>
+                    <button className={styles.navItem}>Services</button>
+                </Link>
+                <Link href='/news'>
+                    <button className={styles.navItem}>News</button>
+                </Link>
+                <Link href='/contact'>
+                    <button className={styles.navItem}>Contact</button>
+                </Link>
+            </div>
         </div>
     ) 
 }
