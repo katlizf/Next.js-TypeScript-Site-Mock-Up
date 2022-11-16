4e30de48792eacba5508b51a6e4a688c67fa15b0
Katie Anderson <katie.fullstackwebdev@gmail.com>
2022-11-16 13:59:03 -0600

    changed data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.css'
 function Footer() {
     return (
         <div className={styles.footer}>
-            <p className={styles.footerItem}>© 2022 Mock-Up, Inc.</p>
+            <p className={styles.footerItem}>© 2022 Tundra Targeted Therapeutics, Inc.</p>
             <p className={styles.footerItem}>|</p>
             <p>email@gmail.com</p>
         </div>
